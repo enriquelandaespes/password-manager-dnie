8087d62c8b3f98f0c8ff78a977c6750113c355e4
enriquelandaespes <enriquelandaespes@gmail.com>
2025-10-06 18:53:36 +0200

    Update generador_contraseñas.py

--- generador_contraseñas.py
+++ generador_contraseñas.py
@@ -19,13 +19,12 @@ def crear_contraseña(longitud):
     
     return contraseña_generada
 
-# --- Programa Principal ---
-
-# Pedimos al usuario que elija la longitud. Se recomienda 12 o más.
+# Generamos una contraseña aleatoria que tenga una longitud segura(Entre 15 y 25 carácteres)
 def generar_contraseña():
     # Elegimos una longitud aleatoria entre 15 y 25
         longitud_deseada = random.randint(15, 25)
         
         # Generamos la contraseña llamando a la función
         contraseña_final = crear_contraseña(longitud_deseada)
+
         return contraseña_final
